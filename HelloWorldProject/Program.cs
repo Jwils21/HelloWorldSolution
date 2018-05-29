b751f3121833fb6eddc561757805f07f04996f1b
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-29 16:36:09 -0400

    Initial commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -7,16 +7,51 @@ using System.Threading.Tasks;
 namespace HelloWorldProject {
 	class Program {
 		static void Main(string[] args) {
-			string YourName;
-			//Console.BackgroundColor=ConsoleColor.Yellow;
 			Console.ForegroundColor = ConsoleColor.Blue;
 			Console.WriteLine("Hello, world!");
 
-			Console.WriteLine("What is your name?");
+			Student student = new Student();
 
-			YourName = Console.ReadLine();
+			student.SetFirstName("Frank");
+			student.SetLastname("Zappa");
+			student.SetStudentID(24601);
+			student.SetGPA(3.8);
+			student.SetSAT(400);
+			student.SetIsFemale(false);
+			
+			string Fname;
+			string Lname;
+			int StID;
+			double StGPA;
+			int StSAT;
+			bool StIsFemale;
 
-			Console.WriteLine("Hello {0}", YourName);
+
+			Fname = student.GetFirstname();
+			Lname = student.GetLastname();
+			StID = student.GetStudentID();
+			StGPA = student.GetGPA();
+			StSAT = student.GetSAT();
+			StIsFemale = student.GetIsFemale();
+
+			Console.WriteLine(Fname + " " + Lname + ", Student ID: " + StID + ", GPA: " + StGPA + ", SAT: " + StSAT + ", IsFemale: " + StIsFemale);
+
+			
+
+			//string YourName;
+			//string DreamJob;
+			//Console.WriteLine("What is your name?");
+			//YourName = Console.ReadLine();
+			//Console.WriteLine("Hello {0}", YourName);
+
+
+			//Console.WriteLine("What is your dream job?");
+			//Console.ForegroundColor = ConsoleColor.DarkRed;
+			//DreamJob = Console.ReadLine();
+			//Console.ForegroundColor = ConsoleColor.White;
+			//Console.WriteLine("{1} your dream job is to be a {0}", DreamJob, YourName);
+
+
 		}
 	}
 }
--- HelloWorldProject/Student.cs
+++ HelloWorldProject/Student.cs
@@ -6,5 +6,55 @@ using System.Threading.Tasks;
 
 namespace HelloWorldProject {
 	class Student {
+		string Firstname;
+		string Lastname;
+		int StudentId;
+		double GPA;
+		int SAT;
+		bool IsFemale;
+
+		public string GetFirstname() {
+			return Firstname;
+		}
+
+		public void SetFirstName(string pFirstName) {
+			Firstname = pFirstName;
+		}
+		public string GetLastname() {
+			return Lastname;
+		}
+
+		public void SetLastname(string pLastName) {
+			Lastname = pLastName;
+		}
+
+		public int GetStudentID() {
+			return StudentId;
+		}
+
+		public void SetStudentID(int StdntID) {
+			StudentId = StdntID;
+		}
+
+		public double GetGPA() {
+			return GPA;
+		}
+
+		public void SetGPA(double pGPA) {
+			GPA = pGPA;
+		}
+
+		public int GetSAT() {
+			return SAT;
+		}
+		public void SetSAT(int pSAT) {
+			SAT = pSAT;
+		}
+		public bool GetIsFemale() {
+			return IsFemale;
+		}
+		public void SetIsFemale(bool pIsFemale) {
+			IsFemale = pIsFemale;
+		}
 	}
 }
