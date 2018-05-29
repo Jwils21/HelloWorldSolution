6857a229792036562ce6182c3de63d2e9a064837
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-29 11:26:36 -0400

    Initial Commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorldProject
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace HelloWorldProject {
+	class Program {
+		static void Main(string[] args) {
+			string YourName;
+			//Console.BackgroundColor=ConsoleColor.Yellow;
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("Hello, world!");
+
+			Console.WriteLine("What is your name?");
+
+			YourName = Console.ReadLine();
+
+			Console.WriteLine("Hello {0}", YourName);
 		}
 	}
 }
